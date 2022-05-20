4596949ddc377b3d91417f8abd36950891ed9713
rzk <rzoq88@gmail.com>
2022-05-20 04:39:32 -0400

    Added my project

--- js/packages/web/src/components/Banner/index.tsx
+++ js/packages/web/src/components/Banner/index.tsx
@@ -55,7 +55,7 @@ export const Banner = (props: {
           {props.children}
           <div className="powered-by">
             <span>
-              POWERED BY <b>METAPLEX</b>
+               <b>OSH Group</b>
             </span>
           </div>
         </div>
